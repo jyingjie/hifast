377ab8cd98af318c1b923be76e8d8c241b7e1ba5
Xu Chen <stellarxu@qq.com>
2021-06-18 03:06:19 +0000

    independent test codes

---
+++ rfi_fft_codes/cli_markRFI.py
@@ -0,0 +1,400 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# author: Xu Chen, Li Fujia, 2021.06
+# code：Xu Chen
+# original code: Jing Yingjie
+
+import sys
+import os
+import re
+
+from astropy import log
+
+import numpy as np
+import h5py
+from copy import deepcopy
+from tqdm import tqdm
+
+
+if __name__ == '__main__':
+    import warnings 
+    warnings.filterwarnings("ignore",r'overflow encountered in exp')
+    warnings.filterwarnings("ignore",r'Polyfit may be poorly conditioned')
+    import argparse
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+    
+    parser.add_argument('fname',
+                        help='file name')
+    parser.add_argument('--outdir',
+                       help='default is same with the input file')
+    parser.add_argument('-f', '--force', action='store_true',
+                        help='overwriting file if out file exists')
+    
+    ## time rfi
+    parser.add_argument('--time_rfi', action='store_true',
+                        help='find time rfi')
+    # short freq
+    parser.add_argument('--sf_frange', type=float, nargs=2,
+                       help='freq range exists short-freq time rfi')
+    parser.add_argument('--sf_times', type=float, default=10,
+                       help='first threhold, rfi is this times of median value')
+    parser.add_argument('--sf_thr', type=float, default=10,
+                       help='sharp edge on time axis. diff above this times of next point will be recognized.')
+    parser.add_argument('--sf_rfi_last',type=float, default=20, 
+                       help='rfi lasts at least 20 spec numbers')
+    parser.add_argument('--sf_T_thr',type=float, default=.5, 
+                       help='T above thr will be masked')
+    # long freq
+    parser.add_argument('--lf_beams', 
+                        help='beam numbers which has long-freq time rfi')
+    parser.add_argument('--lf_frange', type=float, nargs=2,
+                       help='freq range exists long-freq time rfi')
+    parser.add_argument('--lf_times', type=float, default=1.5,
+                       help='first threhold, rfi is this times of median value')
+    parser.add_argument('--lf_thr', type=float, default=0,
+                       help='sharp edge on time axis. diff above this times of next point will be recognized.')
+    parser.add_argument('--lf_rfi_last',type=float, default=20, 
+                       help='rfi lasts at least 20 spec numbers')
+    parser.add_argument('--lf_ext',type = int,default=10, 
+                       help='extend edge')
+
+    #smooth
+    parser.add_argument('--s_method_freq', default='None', choices=['gaussian', 'boxcar', 'median', 'None'],
+                       help='smooth spec along freq')
+    parser.add_argument('--s_sigma_freq', type=int, default=3,
+                       help='smooth spec along freq sigma')
+    parser.add_argument('--s_method_t', default='None', choices=['gaussian', 'boxcar', 'median', 'None'],
+                       help='smooth spec along time')
+    parser.add_argument('--s_sigma_t', type=int, default=10,
+                       help='smooth spec along time sigma')
+    
+    ## freq rfi
+    # find rfi
+    parser.add_argument('--rfi_thr', type=float, default=3, 
+                       help='default 3 times of rms threshold')
+    parser.add_argument('--rms_frange', type=float, nargs=2,
+                       help='freq range to compute rms')
+    parser.add_argument('--mw_frange', type=float, nargs=2,
+                       help='protect milky way freq range(estimate)')
+    
+    parser.add_argument('--rfi_width_lim', type=float, default=10, 
+                       help='rfi should contain more channels than limit')
+    parser.add_argument('--ext_sec', type=int, default=30, 
+                       help='extend channel number of start and end of each section')
+    parser.add_argument('--freq_thr', type=float, default=.5, 
+                       help='estimate error when looking for peaks to polyfit')
+    parser.add_argument('--freq_step', type=float, default = 8.1, 
+                       help='rfi period MHz')
+    parser.add_argument('--rfi_groups', default = 'two groups', choices=['two groups','three groups','all'],
+                        help='divide rfi into 2 or 3 groups')
+    
+    # mask rfi
+    parser.add_argument('--mask_thr', type=float, default=15, 
+                       help='default 15 times of rms threshold will be masked')
+    parser.add_argument('--ext_edge', type=int, default= 0, 
+                       help=' extend result channel on freq axis')
+    parser.add_argument('--mask_all_theory', action= 'store_true',
+                       help='mask_all_theory')
+    parser.add_argument('--freq_from_theory', type=float,default = .5,
+                        help='if mask_all_theory, mask width from theory')
+    parser.add_argument('--time_coherent_per', type=float, default = 1,
+                       help='rfi in one freq appears more than emmm, maybe 70%, mask them all on time axis.')
+    
+    ## flux calibration
+    parser.add_argument('--flux', action='store_true',
+                       help='flux calibration')
+    parser.add_argument('-c', '--cali_fname',
+                       help='quasar calibration file name')
+    
+    parser.add_argument('--keep_rfi', action='store_true',
+                       help='keep rfi')
+    parser.add_argument('--plot', action= 'store_true',
+                       help='plot')
+    parser.add_argument('--ylim', type=float, nargs=2,
+                        help='set ylim in plot')
+    parser.add_argument('--save_rfi_list', action= 'store_true',
+                       help='plot')
+    
+    
+    args = parser.parse_args()
+    fname = args.fname
+    outdir = args.outdir
+
+    time_rfi = args.time_rfi
+    
+    mask_all_theory = args.mask_all_theory
+    freq_from_theory = args.freq_from_theory
+    mask_thr = args.mask_thr
+    
+    find_args = {}
+    find_args['rfi_width_lim'] = args.rfi_width_lim
+    find_args['ext_sec'] = args.ext_sec
+    freq_thr = args.freq_thr
+
+    freq_step = args.freq_step
+    ext_edge = args.ext_edge
+    rfi_groups = args.rfi_groups
+    
+    # flux
+    flux = args.flux
+    cali_fname=args.cali_fname
+    
+    keep_rfi = args.keep_rfi
+    plot = args.plot
+    save_rfi_list = args.save_rfi_list
+    if save_rfi_list:
+        plot = True
+    
+    
+    outparts = ['pdrfi']
+    if mask_all_theory: outparts += ['strict']
+    if flux: outparts += ['flux']
+    
+    outpart = '-' + '_'.join(outparts)
+    if outdir is None: outdir = os.path.dirname(fname)
+    fileout = os.path.join(outdir, '.'.join(os.path.basename(fname).split('.')[:-1]) + f'{outpart}.hdf5')
+    if os.path.exists(fileout):
+        if args.force:
+            print(f"will overwrite the existing out file {fileout}")
+        else:
+            print(f"File exists {fileout}")
+            print('exit... Using -f to overwrite it.')
+            sys.exit()
+    
+    
+    if plot:
+        from matplotlib import pyplot as plt
+        plt.switch_backend('agg')
+        from matplotlib.backends.backend_pdf import PdfPages
+        if not os.path.exists(outdir+'/fig/'):
+            os.mkdir(outdir+'/fig/')
+        pdfname = os.path.join(outdir+'/fig/','.'.join(os.path.basename(fileout).split('.')[:-1])+ '.pdf')
+        pdf = PdfPages(pdfname)
+    else:
+        pdf = None
+
+    # load data
+    import h5py
+    import numpy as np
+    f = h5py.File(fname, 'r')
+    mjd= f['mjd'][()]
+    ind_sort = np.argsort(f['mjd'])
+    if 'T' in f.keys():
+        T = f['T'][()]
+        outfield = 'T'
+    elif 'Ta' in f.keys():
+        T = f['Ta'][()]
+        outfield = 'Ta'
+    elif 'flux' in f.keys():
+        T = f['flux'][()]
+        outfield = 'flux'
+        if flux:
+            raise(ValueError(f'flux already exists, please remove \"--flux\"'))
+    if flux:
+        outfield = 'flux'
+        
+    if len(T.shape) == 3:
+        T = np.mean(T, axis=2, dtype='float64')    
+    
+    T = T[ind_sort]           
+    freq= f['freq'][()]
+    if 'ra' in f.keys():
+        ra= f['ra'][()][ind_sort]
+        dec= f['dec'][()][ind_sort]
+        ra[ra<0] += 360.
+    
+    if 'Header' in f.keys():
+        from collections import OrderedDict
+        header_in= OrderedDict(f['Header'].attrs.items())
+    else:
+        header_in=None
+        
+    T_ori = deepcopy(T)
+    ####################### time RFI ################################
+    t_rfi = np.zeros_like(T,dtype = 'bool')
+    
+    if time_rfi:
+        longf_rfi = True
+        shortf_rfi = True
+        
+        lf_beams = args.lf_beams
+        if lf_beams is not None:
+            m_pos = fname.find('-M')
+            nB = fname[m_pos+2:m_pos+4]
+            if nB not in lf_beams:
+                longf_rfi = False
+            else:
+                print(f"M{nB} in beam {lf_beams}")
+  
+        t_rfi = np.zeros_like(T,dtype = 'bool')
+        from markRFI import mask_time_rfi
+        if shortf_rfi:
+            shortf_args = {}
+            shortf_args['frange'] = args.sf_frange
+            shortf_args['times'] = args.sf_times
+            shortf_args['thr'] = args.sf_thr
+            shortf_args['rfi_width_lim'] = args.sf_rfi_last
+            shortf_args['T_thr'] = args.sf_T_thr
+            
+            s_rfi = mask_time_rfi(T,freq, rtype = 'short-freq',plot = plot,pdf = pdf,**shortf_args)
+            t_rfi = s_rfi
+            
+        if longf_rfi:
+            longf_args = {}
+            longf_args['frange'] = args.lf_frange
+            longf_args['times'] = args.lf_times
+            longf_args['thr'] = args.lf_thr
+            longf_args['rfi_width_lim'] = args.lf_rfi_last
+            longf_args['ext_add'] = args.lf_ext
+            
+            l_rfi = mask_time_rfi(T,freq, rtype = 'long-freq',plot = plot,pdf = pdf,**longf_args)
+            t_rfi = t_rfi | l_rfi
+    
+        if shortf_rfi:
+            T[s_rfi] = 0
+        
+    whole_rfi = np.all(t_rfi,axis = 1)
+    not_rfi_num = np.arange(T.shape[0])[~whole_rfi]
+    is_rfi_num = np.arange(T.shape[0])[whole_rfi]
+
+    if len(is_rfi_num)>0:
+        T = np.delete(T,is_rfi_num,axis = 0)
+        
+    ########################### smooth ###############################3
+    s_method_freq = args.s_method_freq
+    s_sigma_freq = args.s_sigma_freq
+    s_method_t = args.s_method_t
+    s_sigma_t = args.s_sigma_t
+    
+    from hifast.util import smooth1d
+    if  s_method_t in ['gaussian', 'boxcar', 'median']:
+        print('Smooth ing ...')
+        T = smooth1d(T,axis = 0,sigma = s_sigma_t, method = s_method_t)
+    
+    if  s_method_freq in ['gaussian', 'boxcar', 'median']:
+        print('Smooth ing ...')
+        T = smooth1d(T,axis = 1,sigma = s_sigma_freq, method = s_method_freq)
+    
+    if len(is_rfi_num)>0:
+        T_ = np.full(T_ori.shape,np.nan)
+        T_[not_rfi_num,:] = T
+        T = deepcopy(T_)
+        
+    ###################### freq period RFI ##########################    
+
+    #mark RFI  
+    from markRFI import find_RFI, rms
+    rfi_thr = args.rfi_thr
+    rms_frange = args.rms_frange
+    mw_frange = args.mw_frange
+    #print("Calutating RMS.")
+    #RMS = rms(T,freq,rms_frange)
+    #is_rfi = (T > np.full(T.shape[::-1],RMS * rfi_thr).T)
+    if mw_frange is None:
+        protect_use = np.zeros_like(freq,dtype='bool')
+    else:
+        protect_use = (freq>mw_frange[0])&(freq<mw_frange[1])
+    
+    is_rfi = f['is_rfi'][()] if 'is_rfi' in f.keys() else None 
+    
+    if plot:
+        ylim = args.ylim
+        
+        tn = not_rfi_num[0]
+        spec = deepcopy(T[tn,:])
+        
+        RMS = rms(spec,freq,rms_frange)
+        is_rfi_mw = (spec > RMS * rfi_thr)
+        is_rfi_ = is_rfi_mw & (~protect_use)
+        _,theory = find_RFI(spec,freq,is_rfi_,is_rfi_mw,freq_step = freq_step,RMS = RMS,freq_thr = freq_thr,
+                     ext_edge = ext_edge,rfi_fit_use = 'two groups',plot = plot,pdf=pdf,ylim=ylim,
+                     mask_all_theory = mask_all_theory,freq_from_theory = freq_from_theory,mask_thr =mask_thr,**find_args)
+    
+    if is_rfi is None:
+        #is_rfi = is_rfi & (~protect_use)
+        pd_rfi = np.full(T.shape[:2], False, dtype=bool)
+        rfi_freq_list = np.full((T.shape[0],len(theory)+50),np.nan)
+        
+        log.info("Looking for RFI...")
+        for tn in tqdm(range(T.shape[0])):
+            if tn in not_rfi_num:
+                spec = deepcopy(T[tn,:]) 
+                RMS = rms(spec,freq,rms_frange)
+                is_rfi_mw = (spec > RMS * rfi_thr)
+                is_rfi = (spec > RMS * rfi_thr) & (~protect_use)
+                try:
+                    pd_rfi[tn,:],rfi_theory = find_RFI(spec,freq,is_rfi,is_rfi_mw,freq_step = freq_step,RMS = RMS,
+                              freq_thr = freq_thr,ext_edge = ext_edge,rfi_fit_use = rfi_groups ,plot = False,
+                              mask_all_theory = mask_all_theory,freq_from_theory = freq_from_theory,mask_thr =mask_thr,**find_args)
+
+                    if save_rfi_list:
+                        if len(rfi_theory) < rfi_freq_list.shape[1]:
+                            rfi_theory = np.pad(rfi_theory,(0,rfi_freq_list.shape[1] - len(rfi_theory)))
+                        elif len(rfi_theory) > rfi_freq_list.shape[1]:
+                            rfi_theory = rfi_theory[:rfi_freq_list.shape[1]]
+
+                        rfi_freq_list[tn,:] = rfi_theory
+                    
+                except ValueError:
+                    pd_rfi[tn,:] = True
+                    log.warning(f"tn={tn} has a ValueError !")
+                    import traceback
+                    traceback.print_exc()    
+
+            
+        log.info("Finish finding RFI...")
+    else:
+        raise(ValueError(f'is_rfi already exists'))
+    
+    rfi_mask = pd_rfi | t_rfi
+    
+    time_coherent_per = args.time_coherent_per
+    if (time_coherent_per > 0)&(time_coherent_per <1):
+        per_use = (np.sum(rfi_mask,axis = 0)/rfi_mask.shape[0] > time_coherent_per)
+        rfi_mask[:,per_use] = True
+    
+    if not keep_rfi:
+        T_ori[rfi_mask] = np.nan  
+
+    if plot:    
+        print(" 'Wait for plotting patiently, you must.' Master Yoda said")
+        
+        from util import plot_waterfall
+        plot_waterfall(f,data = T_ori, vmin_max=[-.05,.05],cmap='plasma',figsize=(18,5),
+                       title = os.path.basename(fileout).split('.')[:-1],pdf = pdf)
+        
+        pdf.close()
+        log.info(f"Plot to {pdfname}")
+    
+        
+    if flux:
+        from hifast.flux import cali_src
+        nB = int(re.findall(r'-M[0-1][0-9]',fname)[-1][2:])
+        T_ori = cali_src(T_ori, nB, freq, cali_fname, ra=ra, dec=dec, mjd=mjd)
+    
+    # out dict
+    dict_out={}
+
+    if pd_rfi is not None:
+        dict_out['is_rfi'] = rfi_mask
+    dict_out['freq'] = freq
+    if 'ra' in f.keys():
+        dict_out['ra'] = ra
+        dict_out['dec'] = dec
+    dict_out['mjd'] = mjd
+    dict_out[outfield] = T_ori.astype('float32')
+    if save_rfi_list:
+        dict_out['rfi_list'] = rfi_freq_list
+    #save file
+    print('Saving...')
+    from hifast.util import add_extra
+    add_extra(f, dict_out)
+    f.close()
+    from hifast.util import rec_his, save_dict_hdf5
+    header=rec_his(args=args)
+
+    if header_in is not None: header.update(header_in)
+    save_dict_hdf5(fileout, dict_out, header=header)
+    log.info(f"Saved to {fileout}")
+    
---
+++ rfi_fft_codes/util.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+from copy import deepcopy
+
+
+def percent_vminmax(data,percent = None):
+    if percent != None:
+        arr = data.flatten()
+        arr = arr[~np.isnan(arr)]
+        sort_arr = np.sort(arr)
+        left,right = np.around(len(arr)*(1-percent)/2),np.around(len(arr)*(1+percent)/2)
+
+        return sort_arr[int(left)],sort_arr[int(right)]
+    else:
+        return np.nanmin(data),np.nanmax(data)
+
+def _round_up_to_odd_integer(value):
+    c = np.ceil(value)
+    if np.isscalar(c):
+        if c % 2 == 0:
+            r = c + 1
+        else:
+            r = c
+    else:
+        r = np.zeros(c.shape[0])
+        for i in range(c.shape[0]):
+            if c[i] % 2 == 0:
+                r[i] = c[i] + 1
+            else:
+                r[i] = c[i]
+    return r.astype('int')
+
+def plot_waterfall(f,corvel=None,data = None,xtype = 'freq',polar = 'xx',cmap = 'rainbow',per_vmin_max = None,
+                   vmin_max = None,xylim = None,outdir = './',xrange = None,ynsmall = None,
+                   plot = True,pdf = None, figsize=(18,9),title = None,**kwargs):
+    from matplotlib import pyplot as plt
+    
+    if pdf is not None:
+        plt.switch_backend('agg')
+    
+    ind_sort = np.argsort(f['mjd'])
+    restfreq = 1420.405751#7667
+    if xtype == 'freq':
+        x = f['freq'][()]
+    elif xtype == 'vrad':
+        if 'vel' in f.keys():
+            x = f['vel'][()]
+        else:
+            x = 299792.458*(restfreq-freq)/restfreq
+    elif xtype == 'vopt':
+        freq = f['freq'][()]
+        x = 299792.458*(restfreq-freq)/freq
+    
+    if not isinstance(data,np.ndarray):
+        if 'flux' in f.keys():
+            T = f['flux'][()][ind_sort]
+        elif 'Ta' in f.keys():
+            T = f['Ta'][()][ind_sort]
+        if 'T' in f.keys():
+            T = f['T'][()][ind_sort]
+
+        if len(T.shape) == 2:
+            data = T
+        else:
+            if polar == 'xx':                  
+                data = T[:,:,0]
+            elif polar == 'yy':                  
+                data = T[:,:,1]
+    
+    if xrange != None:
+        x1,x2 = np.min(xrange),np.max(xrange)
+        is_use = (x>x1)&(x<x2)
+        x = x[is_use]
+        if data.shape[1] != x.shape[0]:
+            data = data[:,is_use]
+    else:
+        xrange = [x[0],x[-1]]
+        
+    if ynsmall != None:
+        if 'ra' in f.keys():
+            nspec_use = which_specs(f,nsmall = ynsmall,**kwargs)
+        else:
+            nspec_use = which_specs(corvel,nsmall = ynsmall,**kwargs)
+        mjd = f['mjd'][nspec_use]
+        ind_sort2 = np.argsort(mjd)
+        data = data[nspec_use,:][ind_sort2]
+    
+    if plot:
+        extent = (xrange[0],xrange[1],0,data.shape[0])            
+
+        fig,ax = plt.subplots(figsize=figsize)
+
+        if vmin_max != None:
+            im=ax.imshow(data,vmin=vmin_max[0],vmax=vmin_max[1],origin='lower', cmap = cmap,
+                         aspect='auto',extent = extent)
+        else:
+            if per_vmin_max == None:   
+                im=ax.imshow(data,origin='lower', cmap = cmap,aspect='auto',extent = extent)
+            else:
+                #from Fits_Inspect_Overlap.utils import percent_vminmax
+
+                vmin,vmax = percent_vminmax(data,percent = per_vmin_max)
+                im=ax.imshow(data,vmin=vmin,vmax=vmax,origin='lower', cmap = cmap,
+                            aspect='auto',extent = extent)
+
+        ax.set_xlabel(xtype)
+        ax.set_ylabel("specs")
+        #ax.tick_params(labelsize=14)    
+        ax.set_title(title)
+        plt.colorbar(im,pad=.01)
+        plt.tight_layout()
+        if xylim is not None:
+            xlim1,xlim2,ylim1,ylim2 = xylim
+            ax.set_xlim(xlim1,xlim2)
+            ax.set_ylim(ylim1,ylim2)
+
+        if pdf is not None:
+            pdf.savefig();plt.close()
+        else:
+            plt.show()
+    
+    #return data
+    
+def get_data(f,polar='average',xrange = None):
+    ind_sort = np.argsort(f['mjd'])
+    x = f['freq'][()]
+    if 'flux' in f.keys():
+        T = f['flux'][()][ind_sort]
+    elif 'Ta' in f.keys():
+        T = f['Ta'][()][ind_sort]
+    if 'T' in f.keys():
+        T = f['T'][()][ind_sort]
+        
+    if polar=='average':
+        data = np.mean(T,axis = 2)
+    elif polar=='xx':
+        data =T[:,:,0]
+    elif polar=='yy':
+        data =T[:,:,1]
+    elif polar=='merged':
+        data =T
+    if xrange != None:
+        x1,x2 = np.min(xrange),np.max(xrange)
+        is_use = (x>x1)&(x<x2)
+        x = x[is_use]
+        if data.shape[1] != x.shape[0]:
+            data = data[:,is_use]
+    else:
+        xrange = [x[0],x[-1]]   
+    
+    return np.float64(data)
+
