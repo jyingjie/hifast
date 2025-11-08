04784c62bcc7a50f8feb978001923a90db4a6546
Yingjie <2012jyj@gmail.com>
2025-11-08 23:58:24 +0800

    chore: Remove legacy example directory from version control

--- example/M33/First.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fpart="/data/hw1/FAST/3047/M31_Drift_v3_12/20201201/M31_Drift_v3_12_arcdrift-M"
-
-for i in {01..02}
-do
-python -m hifast.sep "${fpart}${i}_W_0001.fits" -d 6 -m 6 -n 1994 --step 5 --frange 1414 1431 \
-                                --smooth poly --s_deg 1 --outdir ./data_v3 --save_p_cal -f
-done
